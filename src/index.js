8209aeea419e57f6c73f4be0229c45a94e1277dd
valentinalupla <valentinalunaplaategrafica@gmail.com>
2024-04-19 12:08:45 +0200

    funcionality finished

--- src/index.js
+++ src/index.js
@@ -39,53 +39,138 @@ app.listen(port, () => {
 //1. GET
 
 app.get('/signos', async (req,res) => {
-  const connection = await getDBconnection();
+    const connection = await getDBconnection();
 
-  //consulta en la base de datos
-  const querysql = "SELECT * FROM signos";
-  const [result] = await connection.query(querysql);
+    //consulta en la base de datos
+    const querysql = "SELECT * FROM signos";
+    const [result] = await connection.query(querysql);
 
-  connection.end();
+    connection.end();
 
-  //envio la respuesta
+    //envio la respuesta
 
-  res.json({
-    info: {count: result.length}, 
-    results: result,
+    res.json({
+      info: {count: result.length}, 
+      results: result,
+    });
   });
-});
 
 //2. GET:ID
 
 app.get('/signos/:id', async (req, res) => {
 
-  //recoge el id 
-const idSigno = req.params.id;
+    //recoge el id 
+    const idSigno = req.params.id;
 
- //consulta en la base de datos
- const connection = await getDBconnection();
- const querysql = "SELECT * FROM signos WHERE id = ?";
- const [result] = await connection.query(querysql, [idSigno]);
+  //consulta en la base de datos
+  const connection = await getDBconnection();
+  const querysql = "SELECT * FROM signos WHERE id = ?";
+  const [result] = await connection.query(querysql, [idSigno]);
 
- connection.end();
+  connection.end();
 
- if(result.length === 0){
+  if(result.length === 0){
 
-    res.status(404).json({
-      success: false,
-      'error': 'no hay ningún signo con ese id',
-}); 
-} else {
-    res.status(200).json({
-      success: true,
-      result: result,
+      res.status(404).json({
+        success: false,
+        'error': 'no hay ningún signo con ese id',
+  }); 
+  } else {
+      res.status(200).json({
+        success: true,
+        result: result,
+    });
+  }
   });
-}
-});
 
 // 3.POST añadir nuevo signo del zodiaco nombre, elemento, descripcion
 
 app.post('/signos', async (req, res) => {
-const data = req.body; 
+    const data = req.body; 
+    const {name, element, description} = data;
 
-})
+    //añadir signo en la base de datos
+    const connection = await getDBconnection();
+    const querysql = "INSERT INTO signos (nombre, elemento, descripcion) VALUES(?,?,?)";
+    const [result] = await connection.query(querysql, [
+      name,
+      element,
+      description,
+    ]);
+
+    connection.end();
+
+    //enviar respuesta a front-end
+    res.status(201).json({
+      success: true,
+      id: result.insertId,
+    });
+    });
+
+
+//PUT  actualizar signo 
+
+app.put('/signos/:id', async(req, res)=> {
+    //recoge el id
+    const idSigno = req.params.id;
+    // recoge los datos 
+    const newData = req.body;
+    const {name, element, description} = newData;
+
+    //actualiza el signo en la base de datos
+    const connection = await getDBconnection();
+    const querysql = "UPDATE signos SET nombre = ?, elemento = ?, descripcion = ? WHERE id = ?";
+    const [result] = await connection.query(querysql, [
+      name, 
+      element,
+      description,
+      idSigno,
+    ]);
+
+    connection.end();
+
+    //enviar respuesta a front-end
+
+    res.status(200).json({
+      success: true,
+    });
+    
+  });
+
+//5.DELETE
+
+app.delete('/signos/:id', async(req,res) => {
+    //recoge el id
+    const idSigno = req.params.id;
+
+    //actualiza el signo en la base de datos
+    const connection = await getDBconnection();
+
+
+    //elimina la receta de mi base de datos
+    const querysql = "DELETE FROM signos WHERE id = ?";
+    const [result] = await connection.query(querysql, [idSigno]);
+  
+    
+    connection.end();
+  
+    //enviar respuesta a front-end
+
+    if(result.affectedRows > 0){
+      res.status(200).json({
+        success: true,
+        message: 'signo eliminado',
+      });
+    } else {
+      res.status(400).json({
+        success: false,
+        message: 'No se ha eliminado el signo',
+      });
+    };
+
+    
+  });
+
+
+
+
